1195bf8d93d440dd4719041da8a536418fef0d4e
jemima <88172123+jssmanuel0809@users.noreply.github.com>
2022-05-17 13:44:01 +0800

    Updated components, updated popular carousel

--- components/footer.js
+++ components/footer.js
@@ -5,165 +5,166 @@ class Footer extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-        <style>
-        footer{
-            width: 100%;
-            color: #d9d9d9;
-        }
-        footer{
-            position: relative;
-            bottom: 0px;
-            width: 100%;
-            background: linear-gradient(to right, #3c1053, #ad5389);
-        }
-        .footer-box{
-            display: flex;
-        }
-        .footer-box .content-box{
-            flex-basis: 50%;
-            padding: 10px 20px;
-        }
-        .content-box h2{
-            font-family: Courier;
-            font-size: 1.125rem;
-            font-weight: 600;
-            text-transform: uppercase;
-        }
-        .content-box .footer-context{
-            margin: 20px 0 0 0;
-            position: relative;
-        }
-        .content-box .footer-context:before{
-            position: absolute;
-            content: '';
-            top: -10px;
-            height: 2px;
-            width: 100%;
-            background: #1a1a1a;
-        }
-        .content-box .footer-context:after{
-            position: absolute;
-            content: '';
-            height: 2px;
-            width: 15%;
-            background: #F582A7;
-            top: -10px;
-        }
-        .social-box .footer-context p{
-            font-family: Courier;
-            text-align: justify;
-        }
-        .social-box .footer-context .social{
-            margin: 20px 0 0 0;
-        }
-        .social-box .footer-context .social a{
-            padding: 0 2px;
-            color: #d9d9d9;
-        }
-        .social-box .footer-context .social a span{
-            height: 40px;
-            width: 40px;
-            background: #711A75;
-            line-height: 40px;
-            text-align: center;
-            font-size: 18px;
-            border-radius: 5px;
-            transition: 0.3s;
-        }
-        .social-box .footer-context .social a span:hover{
-            background: #F10086;
-        }
-        .contact-box .footer-context .fas{
-            font-size: 1.4375rem;
-            background: #711A75;
-            height: 45px;
-            width: 45px;
-            line-height: 45px;
-            text-align: center;
-            border-radius: 50%;
-            transition: 0.3s;
-            cursor: pointer;
-        }
-        .contact-box .footer-context .fas:hover{
-            background: #F10086;
-        }
-        .contact-box .footer-context .text{
-            font-family: Courier;
-            font-size: 1.0625rem;
-            font-weight: 500;
-            padding-left: 10px;
-        }
-        .contact-box .content .phone{
-            margin: 15px 0;
-        }
-        .message-box form .text{
-            font-family: Courier;
-            font-size: 1.0625rem;
-            margin-bottom: 2px;
-            color: white;
-        }
-        .message-box form .msg{
-            margin-top: 10px;
-        }
-        .message-box form input, .message-box form textarea{
-            width: 100%;
-            font-size: 1.0625rem;
-            color: white;
-            background: #333;
-            padding-left: 10px;
-            border: 1px solid #222222;
-        }
-        .message-box form input{
-            height: 35px;
-        }
-        .message-box form .stud{
-            margin-top: 10px;
-        }
-        .message-box form .stud button{
-            height: 40px;
-            width: 100%;
-            border: none;
-            outline: none;
-            background: #F10086;
-            font-size: 1.0625rem;
-            font-weight: 500;
-            cursor: pointer;
-            transition: .3s;
-        }
-        .message-box form .stud button:hover{
-            background:#711A75;
-        }
-        .bottom-footer center{
-            padding: 5px;
-            font-size: 0.9375rem;
-            background: #180A0A;
-        }
-        .bottom-footer center span{
-            color: #656565;
-        }
-        .bottom-footer center a{
-            color: #E9D5DA;
-            text-decoration: none;
-        }
-        .bottom-footer center a:hover{
-            text-decoration: underline;
-        }
-        @media screen and (max-width: 900px) {
-            footer{
-                position: relative;
-                bottom: 0px;
-            }
-            .footer-box{
-                flex-wrap: wrap;
-                flex-direction: column;
-            }
-            .footer-box .content-box{
-                margin: 5px 0;
-            }
-        }
-        </style>
-      
+        <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+            <style>
+                footer{
+                    width: 100%;
+                    color: #d9d9d9;
+                }
+                footer{
+                    position: relative;
+                    bottom: 0px;
+                    width: 100%;
+                    background: linear-gradient(to right, #3c1053, #ad5389);
+                }
+                .footer-box{
+                    display: flex;
+                }
+                .footer-box .content-box{
+                    flex-basis: 50%;
+                    padding: 10px 20px;
+                }
+                .content-box h2{
+                    font-family: Courier;
+                    font-size: 1.125rem;
+                    font-weight: 600;
+                    text-transform: uppercase;
+                }
+                .content-box .footer-context{
+                    margin: 20px 0 0 0;
+                    position: relative;
+                }
+                .content-box .footer-context:before{
+                    position: absolute;
+                    content: '';
+                    top: -10px;
+                    height: 2px;
+                    width: 100%;
+                    background: #1a1a1a;
+                }
+                .content-box .footer-context:after{
+                    position: absolute;
+                    content: '';
+                    height: 2px;
+                    width: 15%;
+                    background: #F582A7;
+                    top: -10px;
+                }
+                .social-box .footer-context p{
+                    font-family: Courier;
+                    text-align: justify;
+                }
+                .social-box .footer-context .social{
+                    margin: 20px 0 0 0;
+                }
+                .social-box .footer-context .social a{
+                    padding: 0 2px;
+                    color: #d9d9d9;
+                }
+                .social-box .footer-context .social a span{
+                    height: 40px;
+                    width: 40px;
+                    background: #711A75;
+                    line-height: 40px;
+                    text-align: center;
+                    font-size: 18px;
+                    border-radius: 5px;
+                    transition: 0.3s;
+                }
+                .social-box .footer-context .social a span:hover{
+                    background: #F10086;
+                }
+                .contact-box .footer-context .fas{
+                    font-size: 1.4375rem;
+                    background: #711A75;
+                    height: 45px;
+                    width: 45px;
+                    line-height: 45px;
+                    text-align: center;
+                    border-radius: 50%;
+                    transition: 0.3s;
+                    cursor: pointer;
+                }
+                .contact-box .footer-context .fas:hover{
+                    background: #F10086;
+                }
+                .contact-box .footer-context .text{
+                    font-family: Courier;
+                    font-size: 1.0625rem;
+                    font-weight: 500;
+                    padding-left: 10px;
+                }
+                .contact-box .content .phone{
+                    margin: 15px 0;
+                }
+                .message-box form .text{
+                    font-family: Courier;
+                    font-size: 1.0625rem;
+                    margin-bottom: 2px;
+                    color: white;
+                }
+                .message-box form .msg{
+                    margin-top: 10px;
+                }
+                .message-box form input, .message-box form textarea{
+                    width: 100%;
+                    font-size: 1.0625rem;
+                    color: white;
+                    background: #333;
+                    padding-left: 10px;
+                    border: 1px solid #222222;
+                }
+                .message-box form input{
+                    height: 35px;
+                }
+                .message-box form .stud{
+                    margin-top: 10px;
+                }
+                .message-box form .stud button{
+                    height: 40px;
+                    width: 100%;
+                    border: none;
+                    outline: none;
+                    background: #F10086;
+                    font-size: 1.0625rem;
+                    font-weight: 500;
+                    cursor: pointer;
+                    transition: .3s;
+                }
+                .message-box form .stud button:hover{
+                    background:#711A75;
+                }
+                .bottom-footer center{
+                    padding: 5px;
+                    font-size: 0.9375rem;
+                    background: #180A0A;
+                }
+                .bottom-footer center span{
+                    color: #656565;
+                }
+                .bottom-footer center a{
+                    color: #E9D5DA;
+                    text-decoration: none;
+                }
+                .bottom-footer center a:hover{
+                    text-decoration: underline;
+                }
+                @media screen and (max-width: 900px) {
+                    footer{
+                        position: relative;
+                        bottom: 0px;
+                    }
+                    .footer-box{
+                        flex-wrap: wrap;
+                        flex-direction: column;
+                    }
+                    .footer-box .content-box{
+                        margin: 5px 0;
+                    }
+                }
+            </style>
+        </head>
 
         <footer>
         <div class="footer-box">
--- components/header.js
+++ components/header.js
@@ -5,85 +5,91 @@ class Header extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-
-      <style>
-      *{
-        font-family: Courier;
-        font-size: 18px;
-        outline: none;
-        border: none;
-        text-decoration: none;
-      }
-      header{
-        width: 100%
-      }
-      .headerlogo{
-        position: fixed;
-        background: #355C7D;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);  /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to right, #C06C84, #6C5B7B, #355C7D); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-        height: 50;
-        width: 100%;
-        top: 0; left: 0; right: 0;
-        text-align: center;
-        font-size: 100%;
-        padding: 15 0 0 0;
-        z-index: 2;
-      }
-      .headerlogo a{
-        color: white;
-        margin-left: 80px;
-      }
-      .righticons{
-        height:20px;
-        float: right;
-        margin-right: 30px;
-      }
-      .headerlogo .logo{
-        height: 30px;
-        margin-left: 125px;
-      }
-      .headerlogo img:hover{
-        cursor: pointer;
-      }
-      .headernav{
-        position: fixed;
-        top: 50; left: 0; right: 0;
-        background: #fff;
-        padding: 1rem 20%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        box-shadow: 0 .6rem 1rem rgba(0, 0, 0, .1);
-        z-index: 2;
-      }
-      .headernav a{
-        color: gray;
-      }
-      .headernav a:hover{
-        color: black;
-      }
-      </style>
+      <head>
+        <style>
+        *{
+          font-family: Courier;
+          font-size: 18px;
+          outline: none;
+          border: none;
+          text-decoration: none;
+        }
+        header{
+          width: 100%
+        }
+        .headerlogo #clock{
+          font-size: 10px;
+          color: black;
+        }
+        .headerlogo{
+          position: fixed;
+          background: #355C7D;  /* fallback for old browsers */
+          background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);  /* Chrome 10-25, Safari 5.1-6 */
+          background: linear-gradient(to right, #C06C84, #6C5B7B, #355C7D); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+          height: 50;
+          width: 100%;
+          top: 0; left: 0; right: 0;
+          text-align: center;
+          font-size: 100%;
+          padding: 15 0 0 0;
+          z-index: 2;
+        }
+        .headerlogo a{
+          color: white;
+          margin-left: 80px;
+        }
+        .righticons{
+          height:20px;
+          float: right;
+          margin-right: 30px;
+        }
+        .headerlogo .logo{
+          height: 30px;
+          margin-left: 125px;
+        }
+        .headerlogo img:hover{
+          cursor: pointer;
+        }
+        .headernav{
+          position: fixed;
+          top: 50; left: 0; right: 0;
+          background: #fff;
+          padding: 1rem 20%;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          box-shadow: 0 .6rem 1rem rgba(0, 0, 0, .1);
+          z-index: 2;
+        }
+        .headernav a{
+          color: gray;
+        }
+        .headernav a:hover{
+          color: black;
+        }
+        </style>
+      </head>
 
       <header>
             <!-- HEADER - LOGO, LOGIN, CART-->
           <div class="headerlogo">
-              <a href="index.html"><img class="logo" src="../public/image/logotextwhite.png"></a>
-              <a href="#login"><img class="righticons" src="../public/image/user.png"></a>
-              <a href=""><img class="righticons" src="../public/image/cart.png"></a>
+            <a href="index.html"><img class="logo" src="../public/image/logotextwhite.png"></a>
+            <a href="#login"><img class="righticons" src="../public/image/user.png"></a>
+            <a href=""><img class="righticons" src="../public/image/cart.png"></a>
           </div>
 
             <!-- HEADER - NAV -->
           <div class="headernav">
-              <a href="../pages/index.html">HOME</a>
-              <a href="../pages/shop.html">SHOP ALL</a>
-              <a href="../pages/apparel.html">APPARELS</a>
-              <a href="../pages/accessories.html">ACCESSORIES</a>
-              <a href="../pages/books.html">BOOKS</a>
-              <a href="../pages/faqs.html">FAQS</a>
-              <a href="../pages/about.html">ABOUT</a>
+            <a href="../pages/index.html">HOME</a>
+            <a href="../pages/shop.html">SHOP ALL</a>
+            <a href="../pages/apparel.html">APPARELS</a>
+            <a href="../pages/accessories.html">ACCESSORIES</a>
+            <a href="../pages/books.html">BOOKS</a>
+            <a href="../pages/faqs.html">FAQS</a>
+            <a href="../pages/about.html">ABOUT</a>
           </div>
-        </header>
+      </header>
+
         `;
   }
 }
