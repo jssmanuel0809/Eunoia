f9af07005e98f09e6bd25503e8711ffdcd975ec9
jemima <88172123+jssmanuel0809@users.noreply.github.com>
2022-05-19 15:18:56 +0800

    Update email.js

--- script/email.js
+++ script/email.js
@@ -1,5 +1,6 @@
 function sendEmail(){
     Email.send({
+        // SecureToken : "9160d7bd-7612-467b-8c8e-8b7415c3a813",
         Host : "smtp.elasticemail.com",
         Username : "developmentpurp@gmail.com",
         Password : "CE16D0FBE2A1048AEF96957988FEA37142C8",
